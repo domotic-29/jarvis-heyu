c282d4acb4770671d5e1b215c3b917703ad542da
domotic-29 <30129045+domotic-29@users.noreply.github.com>
2017-11-15 18:45:53 +0100

    Add files via upload

---
+++ functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pg_jh_lg() {
+    case "$1" in
+        no_device_matching) echo "Aucun appareil ne correspond à $2";;
+        switching_on) echo "J'allume $2";;
+        switching_off) echo "J'éteins $2";;
+        *) jv_error "ERROR: unknown translation key: $1";;
+    esac
+}
+
